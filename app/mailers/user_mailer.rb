3a54916682fab82fbf23bc67e12bf3a04b7238e8
Javier Noris <norisjavier@gmail.com>
2014-08-11 14:21:40 -0700

    getting slug size down

--- app/mailers/user_mailer.rb
+++
@@ -1,9 +0,0 @@
-class UserMailer < ActionMailer::Base
-  default from: "info@sfpourhouse.com"
-
-  def welcome_email(user)
-    @user = user
-    @url  = 'http://example.com/login'
-    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
-  end
-end
